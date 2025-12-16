424a21f75f01a33e4721c5a13c21cc16f09eb725
Sahil <sk950121@gmail.com>
2025-12-16 11:17:12 +0530

    feat: add Gitleaks configuration and pre-commit hook for secret scanning; update ESLint rules and dependencies in package.json and package-lock.json

--- app/(components)/Content.tsx
+++ app/(components)/Content.tsx
@@ -5,23 +5,17 @@ import { tools } from "../utils/tools";
 import Header from "../(components)/Header";
 
 export default function Content() {
-    return (
-        <main className="pb-8 min-h-screen pt-10">
-            {/* Header */}
-            <div className={appContent({ padding: "sm" })}>
-                <Header />
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6 px-4 sm:px-0">
-                    {tools.map((tool) => (
-                        <Cards
-                            key={tool.title}
-                            title={tool.title}
-                            description={tool.description}
-                            link={tool.link}
-                            icon={tool.icon}
-                        />
-                    ))}
-                </div>
-            </div>
-        </main>
-    );
+  return (
+    <main className="pb-8 min-h-screen pt-10">
+      {/* Header */}
+      <div className={appContent({ padding: "sm" })}>
+        <Header />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6 px-4 sm:px-0">
+          {tools.map((tool: any) => (
+            <Cards key={tool.title} title={tool.title} description={tool.description} link={tool.link} icon={tool.icon} />
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 }
